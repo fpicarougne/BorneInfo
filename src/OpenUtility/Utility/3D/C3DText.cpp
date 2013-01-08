2d10e95fb86d73915e36b50ca1237ebdc8235197
Fabien Picarougne <fabien.picarougne@univ-nantes.fr>
2013-01-08 15:19:25 +0100

    Fontloader update #1

--- src/OpenUtility/Utility/3D/C3DText.cpp
+++ src/OpenUtility/Utility/3D/C3DText.cpp
@@ -19,12 +19,21 @@ OpenUtility::C3DText& OpenUtility::C3DText::operator=(const C3DText &obj)
 	return(*this);
 }
 
-void OpenUtility::C3DText::SetText(const char *text)
+void OpenUtility::C3DText::SetText(const char *text,EHAlign hAlign,EVAlign vAlign)
 {
+	SetAlignement(hAlign,vAlign);
+	UpdateText(text);
+}
+
+void OpenUtility::C3DText::UpdateText(const char *text)
+{
+	Text=text;
 }
 
 void OpenUtility::C3DText::SetAlignement(EHAlign hAlign,EVAlign vAlign)
 {
+	CurrentHAlign=hAlign;
+	CurrentVAlign=vAlign;
 }
 
 void OpenUtility::C3DText::DrawText()
--- src/OpenUtility/Utility/3D/C3DText.h
+++ src/OpenUtility/Utility/3D/C3DText.h
@@ -2,6 +2,7 @@
 	#define _C3DText_h
 
 #include "CFontEngine.h"
+#include "../Stream/Stream.h"
 
 namespace OpenUtility
 {
@@ -13,9 +14,15 @@ public:
 	C3DText(const C3DText &obj);
 	~C3DText();
 	C3DText& operator=(const C3DText &obj);
-	void SetText(const char *text);
+	void SetText(const char *text,EHAlign hAlign=EHAlignLeft,EVAlign vAlign=EVAlignBaseligne);
+        void UpdateText(const char *text);
 	void SetAlignement(EHAlign hAlign=EHAlignLeft,EVAlign vAlign=EVAlignBaseligne);
 	void DrawText();
+
+private:
+    CStream Text;
+    EHAlign CurrentHAlign;
+    EHAlign CurrentVAlign;
 };
 
 }
